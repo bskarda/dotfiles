f138239340009640c11afb9b292e8650826e655f
Brian Skarda <brian.skarda@gmail.com>
2018-08-15 16:28:04 -0400

    Mirror stash layout

--- bin/clone_all_stash_projects.sh
+++ bin/clone_all_stash_projects.sh
@@ -10,5 +10,8 @@ echo
 PROJECTS=$(curl -L -k -s https://stash//rest/api/1.0/projects?limit=1000 | jq -r '.values | map (.key) | .[]')
 
 for project in $PROJECTS; do
+  mkdir -p "$project"
+  pushd $project
   curl -k --user "$USER":"$password" https://stash/rest/api/1.0/projects/"$project"/repos?limit=2000 | jq --raw-output '.values | map(.links.clone | map(select(.name == "ssh")))[][] | .href ' | xargs -I {} git clone {}
+  popd
 done
